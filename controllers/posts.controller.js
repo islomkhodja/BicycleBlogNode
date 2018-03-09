fd328a7c7ef862f51187a75e9eac387d76cbf1be
Islomkhodja Hamidullakhodjaev <hamidullakhodjaev@gmail.com>
2018-03-10 00:05:36 +0500

    deleted comment

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -4,27 +4,6 @@ const chalk = require('chalk')
 
 exports.getPostBySlug = async (req, res, next) => {
 	Posts.getPostBySlug(req.params.slug)
-		.then(post => {
-			res.render('article', { post });
-		})
-		.catch(err => {
-			console.log(err);
-			next(err);
-		})
-
-	// await Promise.all([
-	// 	models.posts.getPostsBySlug(url_slug),
-	// 	models.terms_relationship.getAllTermsByPostId(_id)
-	// ]).then((data) => {
-	// 	if(data) {
-	// 		let posts = data[0];
-	// 		let terms = data[1];
-	// 		let result = postProcessing(posts, terms);
-
-	// 		res.locals.posts = result;			
-	// 	}
-	// 	return res.render("article");
-	// }).catch(err => {
-	// 	return next(err);
-	// })
+		.then( post => res.render('article', { post }) );
+		.catch(err => next(err))
 }
